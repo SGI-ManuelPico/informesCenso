3cbffff41b45ee8ec04ac8f22903f587129a0318
PabloAOjedaG <p.ojeda@uniandes.edu.co>
2024-07-26 12:01:34 -0500

    First actual commit.

---
+++ main.py
@@ -0,0 +1,8 @@
+from persistence.informePrimero import InformePrimero
+
+
+def main():
+    InformePrimero
+
+if __name__== "__main__":
+    main()
---
+++ persistence/informePrimero.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+
+class InformePrimero:
+    def __init__(self):
+        pass
+
+    def crearArchivoPrimero(self):
+
+        archivoPrimero = pd.read_excel()
+        
