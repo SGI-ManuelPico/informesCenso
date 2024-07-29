966f053ce656fe3a97a6098d1df9d7939125d28c
PabloAOjedaG <p.ojeda@uniandes.edu.co>
2024-07-29 10:56:09 -0500

    Third form completed.

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-from persistence.informeSegundo import InformeTercero
+from persistence.informeTercero import InformeTercero
 
 
 def main():
-    InformeTercero
+            
+            archivoInicial = InformeTercero().lecturaArchivoTercero()
+print(a)
+rutaArchivoFormato = os.getcwd() + "\\censos\\FORMATO 3 COMERCIAL - Aprobado.xlsx"
+
+            for index, row in archivoInicial.iterrows():
+            rutaFormato = os.getcwd()
+            wb = load_workbook(formato)
+            ws = wb.active
+            
+            llenar_form(ws, row)
+            
+        
+            output_path = f"{direc_guardado}form_lleno_{index + 1}.xlsx"
+            wb.save(output_path)
 
 if __name__== "__main__":
     main()
